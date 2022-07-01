963a3019d9eddd1ff331f326af43d15b193822fd
Amer Wafiy <86043171+amerwafiy@users.noreply.github.com>
2022-07-01 09:10:36 +0800

    Change inner join to left join

--- monitoring.py
+++ monitoring.py
@@ -101,7 +101,7 @@ if option1 == 'Daily':
                         df2 = clean_names(df2, 'Agent')
 
                         if isinstance(a, pd.DataFrame) and isinstance(b, pd.DataFrame):
-                            df = pd.merge(df1,df2,on='Agent',how='inner')
+                            df = pd.merge(df1,df2,on='Agent',how='left')
                             df['Calls Attempted'] = [int(x) if math.isnan(x) == False else 0 for x in df['Calls Attempted']]
                             df['CR'] = [int(x) if math.isnan(x) == False else 0 for x in df['CR']]
                             df['Calls-CR'] = df['Calls Attempted'] - df['CR']
